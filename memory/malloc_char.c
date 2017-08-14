af3618bc240970f13c9bb340a8e338a2e2df9e7e
jimarch <jimmy.ilws@gmail.com>
2017-08-13 23:14:37 -0300

    [memory] now RAM (physical memory) is actually used. In fact, the VIRT field (in htop) still is the same, but now the RES field is ~512MB, which is expected since I'm filling the half of the array.

--- memory/malloc_char.c
+++ memory/malloc_char.c
@@ -2,11 +2,14 @@
 #include <stdlib.h>
 /* From here:
  * https://www.darkcoding.net/software/resident-and-virtual-memory-on-linux-a-short-example/
+ *
+ * Compile:
+ * gcc -Wall -g malloc_char.c -o malloc_char.x
  */
 
 void fill(unsigned char* addr, size_t amount) {
     unsigned long i;
-    for (i = 0; i < amount; i++) {
+    for (i = 0; i < amount/2; i++) {
         *(addr + i) = 42;
     }
 }
@@ -18,7 +21,7 @@ int main(int argc, char **argv) {
 
     result = malloc(s);
     printf("Addr: %p\n", result);
-    //fill(result, s);
+    fill(result, s);
 
     scanf("%c", &input);
     return 0;
